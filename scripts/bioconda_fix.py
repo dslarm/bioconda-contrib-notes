386205865ff4f6b45f41b41c5a18826af17f2870
David Lecomber <david+dslarm@lecomber.net>
2025-04-21 14:26:44 +0100

    add more osx-arm64 build

--- scripts/bioconda_fix.py
+++ scripts/bioconda_fix.py
@@ -15,7 +15,13 @@ pin_pattern = re.compile('pin_subpackage')
 version_pattern = re.compile('version: (.+)')
 build_number_pattern = re.compile(r'number: (\d+)')
 generic = re.compile("noarch: generic")
+
+osx_too = 'OSX_TOO' in os.environ
+
 for i in sys.argv[1:]:
+
+         has_aarch64 = False
+         
          try:
                   with open(f'recipes/{i}/meta.yaml', 'r') as f:
                            cont = f.read()
@@ -24,6 +30,11 @@ for i in sys.argv[1:]:
                                     continue
                            if platforms.search(cont):
                                     print (f'{i} has aarch64 support')
+                                    has_aarch64 = True
+                                    if not osx_too:
+                                             continue
+                           if platforms_osx.search(cont):
+                                    print (f'{i} has osx-arm64')
                                     continue
                            
                            if not set_name_pattern.search(cont):
@@ -50,7 +61,10 @@ for i in sys.argv[1:]:
                                              
                            archline = "extra:\n  additional-platforms:\n    - linux-aarch64\n"
 
-                           if 'OSX_TOO' in os.environ:
+                           if osx_too and has_aarch64:
+                                    archline += "    - osx-arm64\n"
+                                    plats = 'osx-arm64'
+                           elif osx_too:
                                     archline += "    - osx-arm64\n"
                                     plats = 'osx-arm64, linux-aarch64'
                            else:
@@ -58,6 +72,8 @@ for i in sys.argv[1:]:
                            
                            if not extra.search(cont):
                                     cont += archline
+                           elif osx_too and has_aarch64:
+                                    cont = re.sub(platforms, '- linux-aarch64\n    - osx-arm64', cont)
                            else:
                                     cont = re.sub(extra, archline, cont)
 
@@ -69,8 +85,8 @@ for i in sys.argv[1:]:
 
                            f.close()
                            print(f'making new recipe {i}')                           
-                           subprocess.run(['git','checkout','-b', f'{i}-aarch64'])
-                           subprocess.run(['git','checkout', f'{i}-aarch64'])
+                           subprocess.run(['git','checkout','-b', f'{i}-{"aarch64" if not has_aarch64 else "osx-arm64"}'])
+                           subprocess.run(['git','checkout', f'{i}-{"aarch64" if not has_aarch64 else "osx-arm64"}'])
                            print(f'subprocess ..')                           
                            with open(f'recipes/{i}/meta.yaml', 'w') as f:
                                     f.write(cont)
