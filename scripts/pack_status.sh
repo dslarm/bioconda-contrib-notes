a60c056d075c87b084704ee441655dc7ebc47990
David Lecomber <david.lecomber@arm.com>
2025-04-28 19:54:00 +0100

    ditto

--- scripts/pack_status.sh
+++ scripts/pack_status.sh
@@ -5,7 +5,7 @@ ORIG=$PWD
 if [ $1 ]; then
     FILE=$1
     sort -k2 -r -g $FILE > $FILE.sorted
-    FILE=$file1.sorted
+    FILE=$file1
 else
     DIR=`mktemp -d /tmp/conda-stats-XXXX`
     echo $DIR
