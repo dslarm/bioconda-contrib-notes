d592d4ae0ea0f2f3717c555c56e20e9f0bc28026
dslarm <david.lecomber@arm.com>
2025-07-02 00:45:53 +0100

    generate packages.md

--- scripts/update.py
+++ scripts/update.py
@@ -12,7 +12,7 @@ os.system("git checkout data")
 
 
 filename="package-downloads/anaconda.org/bioconda/channel.tsv"
-
+packages = "package-downloads/anaconda.org/bioconda/platforms"
 with open(filename, 'r') as f:
     line = f.readline()
     fields = line.rstrip().split('\t')
@@ -74,3 +74,75 @@ with open(filename, 'r') as f:
     #    plt.show()
     plt.savefig('../percents.png')
 
+
+arch_count = {}
+for root, dir, files in os.walk(packages):
+    for filename in files:
+        with open(os.path.join(packages, filename), 'r') as f:
+            line = f.readline()
+            fields = line.rstrip().split('\t')
+            data = {field : [] for field in fields}
+            for line in f:
+                if line.startswith("subdir"):
+                    pass
+                else:
+                    arch = line.split("\t")[0]
+                    count = line.split("\t")[1]
+                    if not (arch in arch_count):
+                        arch_count[arch] = {}
+                    arch_count[arch] [filename.split(".")[0]] = int(count)
+
+os.system("git checkout HEAD~7")
+for root, dir, files in os.walk(packages):
+    for filename in files:
+        with open(os.path.join(packages, filename), 'r') as f:
+            line = f.readline()
+            fields = line.rstrip().split('\t')
+            data = {field : [] for field in fields}
+            for line in f:
+                if line.startswith("subdir"):
+                    pass
+                else:
+                    arch = line.split("\t")[0]
+                    count = line.split("\t")[1]
+                    filebase = filename.split(".")[0]
+                    if not (arch in arch_count):
+                        arch_count[arch] = {}
+                    if filebase in arch_count[arch]:
+                        arch_count[arch][filebase] -= int(count)
+
+
+with open("../packages.md", "w") as f:
+
+    print("# Top 10 packages by architecture", file = f)
+    
+    s = {}
+    f.write('|')
+    for arch in arch_count.keys():
+        s[arch] = sorted(arch_count[arch].items(), key=lambda x: x[1], reverse = True)
+        f.write(f'{arch} | ')
+    print('', file = f)
+
+    for i in range(0, 9):
+        f.write('|')
+        for arch in arch_count.keys():
+            f.write(f'{s[arch][i]} |')
+        print('', file = f)
+
+    print("# First 10 missing packages in linux-aarch64 by linux-x86_64 rank", file = f)
+    counter = 0 
+    for i in s['linux-64']:
+        if not (i[0] in arch_count['linux-aarch64']) and not(i[0] in arch_count['noarch']):
+            print(i, file = f)
+            counter += 1
+        if counter >= 10:
+            break
+        
+    print("# First 10 missing packages in osx-arm64 by osx-64 rank", file = f)
+    counter = 0 
+    for i in s['osx-64']:
+        if not (i[0] in arch_count['osx-arm64']) and not(i[0] in arch_count['noarch']):
+            print(i, file = f)
+            counter += 1
+        if counter >= 10:
+            break
